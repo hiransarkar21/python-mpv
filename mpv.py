0eecbebd5d267e5fca8547b309ae46ae666467fb
Hiran Sarkar <63890677+hiransarkar21@users.noreply.github.com>
2025-05-21 17:22:00 +0530

    added elif block to handle libmpv1 package.

--- mpv.py
+++ mpv.py
@@ -59,6 +59,19 @@ if os.name == 'nt':
         else:
             raise OSError(f'ctypes.find_library found mpv.dll at {dll}, but ctypes.CDLL could not load it.') from e
     fs_enc = 'utf-8'
+    
+elif os.name == "posix":
+    print(">>> OS filesystem - Posix")
+    
+    import locale
+    lc, enc = locale.getlocale(locale.LC_NUMERIC)
+    # libmpv requires LC_NUMERIC to be set to "C". Since messing with global variables everyone else relies upon is
+    # still better than segfaulting, we are setting LC_NUMERIC to "C".
+    locale.setlocale(locale.LC_NUMERIC, 'C')
+    backend = CDLL("libmpv.so.1")
+    fs_enc = sys.getfilesystemencoding()
+    print(">>> Backend - ", backend)
+
 
 else:
     import locale
